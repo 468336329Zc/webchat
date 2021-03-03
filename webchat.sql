91366f3a0525fcb9f06160c64ea2aa8ce1dd2837
468336329Zc <468336329@qq.com>
2021-03-03 21:17:20 +0800

    数据库sql文件

--- webchat.sql
+++ webchat.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: chat
 -- ------------------------------------------------------
--- Server version	5.5.62
+-- Server version	5.5.6
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
